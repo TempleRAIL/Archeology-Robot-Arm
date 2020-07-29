b384e79b5bf0dd7fa6a8cb9b85e87e9172a7b920
Deb Wang <deborah.wang@gmail.com>
2020-07-29 19:32:27 -0400

    Print out mass acquired by get_mass_fun

--- scripts/AutoCore.py
+++ scripts/AutoCore.py
@@ -157,7 +157,7 @@ class AutoCore():
             raise
         else:
             self.mass = -(res.weight/9.8) # [kg]
-            rospy.loginfo('AutoCore: Got sherd mass [kg]')
+            rospy.loginfo('AutoCore: Got sherd mass: {}'.format(self.mass)' kg.')
             return self.mass
         
 
