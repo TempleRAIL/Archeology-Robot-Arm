258ef03bc7f79f702c7754ac0db4b8b786c73fdd
Deb Wang <deborah.wang@gmail.com>
2020-09-08 19:22:33 -0400

    generate color masks that contain multiple colors at given station

--- scripts/color_mask_server.py
+++ scripts/color_mask_server.py
@@ -5,12 +5,14 @@ import cv2
 from sklearn.cluster import KMeans
 from collections import Counter
 from skimage.color import rgb2lab, rgb2hsv, deltaE_cie76
+import colorsys
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 import sys
 import threading
 import math
+import operator
 
 # Import ROS libraries and message types
 import rospy
@@ -44,25 +46,54 @@ def camera_data_callback(im_msg, pc_msg):
         camera_data_lock.release()
 
 ##############################################################
-# RGB2HEX(color)
-# This function converts RGB colors to hex labels for the pie chart.
-# inputs: RGB value stored in 1 x 3 array
-# outputs: hex string
+# HSV2RGB(color)
+# This function converts HSV colors to RGB colors for display in pie chart. (Compatability with matplotlib)
+# inputs: HSV value (OpenCV format) stored in 1 x 3 array
+# outputs: RGB value
 
-def RGB2HEX(color):
-    return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
+def HSV2RGB(color):
+    return colorsys.hsv_to_rgb(color[0]/180, color[1]/255, color[2]/255)
 
 ##############################################################
-# convert_hsv_to_OpenCV(list)
-# This function converts HSV colors from the standard range into the OpenCV range.
-# inputs: list of HSV channels in float format: [decimal of 360 for hue], [decimal % saturation], [decimal % value]
-# returns: list of HSV channels in 0-180 (hue) and 0-255 (saturation, value) ranges
+# get_color_as_string(color)
+# This function returns string labels for the pie chart.
+# inputs: color value as 1x3 array
+# outputs: color value as string
 
-def convert_hsv_to_OpenCV(float_hsv_values):
-    hsv_in_OpenCV = [int(float_hsv_values[0]*180), int(float_hsv_values[1]*255), int(float_hsv_values[2]*255)]
-    
-    return hsv_in_OpenCV
-  
+def get_color_as_string(color):
+    return "({}, {}, {})".format(int(color[0]), int(color[1]), int(color[2]))
+
+##############################################################
+# get_mask_as_ROS_msg(num_colors, cluster_labels, image)
+# This function generates a ROS-msg friendly list of floors and ceilings defining a color mask for each color extracted by KMeans
+# inputs: number of colors, cluster_labels = output of KMeans fit_predict method, OpenCV HSV image used to generate color mask
+
+# returns: flat list of floors and ceilings for each color mask
+
+def get_mask_as_ROS_msg(num_colors, cluster_labels, image):
+    flat_mask = []
+    minval = .07
+    maxval = 1
+
+    for i in range(num_colors):
+        cluster_indices = np.where(labels == i)  # in array of cluster numbers, find every position that belongs to ith cluster
+        colors_in_cluster = reshaped_hsv[cluster_indices]  # cross-reference: get array of all HSV colors belonging to cluster i
+
+        huethresh = np.std(colors_in_cluster[:,0])/2
+        satthresh = np.std(colors_in_cluster[:,1])/2
+
+        minhue = center_colors[i][0] - huethresh
+        maxhue = center_colors[i][0] + huethresh
+
+        minsat = center_colors[i][1] - satthresh
+        maxsat = center_colors[i][1] + satthresh
+
+        floor = [minhue, minsat, minval]	# lower bound of mask for ith color
+        ceiling = [maxhue, maxsat, maxval]	# upper bound of mask ith color
+        flat_mask += (floor+ceiling)  # combine floors and ceilings of all colors into flat list for ROS message
+
+    return flat_mask
+
 ##############################################################
 # image_callback(msg)
 # This function detects and extracts a user-defined number of the most dominant colors in an image.
@@ -75,7 +106,6 @@ def image_callback(msg):
         image_msg = msg
     finally:
         image_msg_lock.release()
-
       
 ##############################################################
 # color_mask_callback(req)
@@ -86,101 +116,64 @@ def image_callback(msg):
 def color_mask_callback(req):
     global image_msg, point_cloud_msg, camera_data_lock
     
-    if not req.num_colors == 1:
-        rospy.logerr('Only 1 color supported')
-        return
+    rospy.loginfo("Extracting {} colors for this station's color mask."format(req.num_colors))
     
     if image_msg is None:
         rospy.logerr('No image received yet')
         return
-
-    #print ("Extracting top ", req.num_colors, " colors.")
-    
+  
     camera_data_lock.acquire()
     try:
-        img = bridge.imgmsg_to_cv2(image_msg, "bgr8")  # BGR OpenCV image
+        temp_img = bridge.imgmsg_to_cv2(image_msg, "bgr8")  # BGR OpenCV image - no HSV encoding available for in cv_bridge
+        hsv_img = cv2.cvtColor(temp_img, cv2.COLOR_BGR2HSV)  # convert to HSV for KMeans clustering
         # convert PointCloud2 ROS msg to numpy array (cloud corresponds to segmented sherds image)
-        point_cloud = ros_numpy.numpify( point_cloud_msg )  # pointcloud should be dense (2D data structure corresponding to image)
+        # pointcloud should be dense (2D data structure corresponding to image)
+        point_cloud = ros_numpy.numpify( point_cloud_msg )  
     finally:
         camera_data_lock.release()
 
-    rgb = img[:, :, ::-1]  # flip to RGB
-    resized_rgb = cv2.resize(rgb, (600,400), interpolation = cv2.INTER_AREA)
-    reshaped_rgb = resized_rgb.reshape(resized_rgb.shape[0]*resized_rgb.shape[1], 3)  # KMeans needs input of 2 dimensions
+    resized_hsv = cv2.resize(hsv_img, (600,400), interpolation = cv2.INTER_AREA)  # downsampling
+    reshaped_hsv = resized_hsv.reshape(resized_hsv.shape[0]*resized_hsv.shape[1], 3)  # KMeans needs input of 2 dimensions
 
-    # Create clusters of colors, extract into labels, and order colors in RGB and hex
+    # Create clusters of colors, extract into labels, and order colors in HSV
     clf = KMeans(n_clusters = req.num_colors) 
-    labels = clf.fit_predict(reshaped_rgb)
+    labels = clf.fit_predict(reshaped_hsv) # numpy array of which cluster each sample belongs to (e.g. [0, 0, 2, 1, 2, 1, 0...])
     counts = Counter(labels)
-    center_colors = clf.cluster_centers_
+    center_colors = clf.cluster_centers_ # HSV coordinates for each cluster center
     ordered_colors = [center_colors[i] for i in counts.keys()] 
     #print ("Ordered colors: ", ordered_colors)
-    hex_colors = [RGB2HEX(ordered_colors[i]) for i in counts.keys()]
+    color_strings = [get_color_as_string(ordered_colors[i]) for i in counts.keys()]
 
+    # Show pie chart of extracted colors
     if (req.show_chart):
-    	print ("Close pie chart to continue.")
-    	plt.figure(figsize = (8, 6))
-    	plt.pie(counts.values(), labels = hex_colors, colors = hex_colors)
-    	plt.show()
+        pie_chart_colors = [HSV2RGB(ordered_colors[i]) for i in counts.keys()]
+        print ("Close pie chart to continue.")
+        plt.figure(figsize = (8, 6))
+        plt.pie(counts.values(), labels = color_strings, colors = pie_chart_colors)
+        plt.show()
 
-    # Create image from ordered colors
-    img_to_cvt = np.zeros((req.num_colors,1,3),np.uint8)
-    
-    for i in range( len(ordered_colors) ): 
-    	for j in range( len(ordered_colors[i]) ):
-    	    img_to_cvt[i][0][j] = ordered_colors[i][j]
-    
-    hsv = rgb2hsv(img_to_cvt)  # convert image to HSV
-    
-    #print ("HSV image of top colors: ", hsv)    
-
-    # Set bounds of color mask
-    huethresh = 0.05
-    satthresh = 0.28
-    
-    minhue = min(hsv[:,:,0]) - huethresh
-    maxhue = max(hsv[:,:,0]) + huethresh
-    
-    minsat = min(hsv[:,:,1]) - satthresh
-    maxsat = max(hsv[:,:,1]) + satthresh
-
-    minval = .07
-    maxval = 1
-
-    floor = convert_hsv_to_OpenCV([minhue, minsat, minval])	# lower bound of mask
-    ceiling = convert_hsv_to_OpenCV([maxhue, maxsat, maxval])	# upper bound of mask
-
-    #print ("Floor: ", floor)
-    #print ("Ceiling: ", ceiling)
-
-    # Combine floor and ceiling into flat list for ROS message
-    flat_mask = floor + ceiling
-
-    #print ('Flat mask: ', flat_mask)
-
+    flat_mask = get_mask_as_ROS_msg(req.num_colors, labels, reshaped_hsv)
 
     res = ColorMaskResponse()
-    res.mat_z = np.average(point_cloud['z']) # get average z value of top face of mat
+    #res.mat_z = np.average(point_cloud['z']) # get average z value of top face of mat
 
     # Construct message for color mask
-    # TODO set this up for more than a single color
     res.color_mask.data = flat_mask
     res.color_mask.layout.dim = [MultiArrayDimension(), MultiArrayDimension(), MultiArrayDimension()]
     
-    res.color_mask.layout.dim[0].label = "height"
-    res.color_mask.layout.dim[0].size = 2
-    res.color_mask.layout.dim[0].stride = 3*1*2
-    res.color_mask.layout.dim[1].label = "width"
-    res.color_mask.layout.dim[1].size = 1
-    res.color_mask.layout.dim[1].stride = 3*1
-    res.color_mask.layout.dim[2].label = "channels"
+    res.color_mask.layout.dim[0].label = "color"
+    res.color_mask.layout.dim[0].size = req.num_colors
+    res.color_mask.layout.dim[0].stride = req.num_colors*2*3
+    res.color_mask.layout.dim[1].label = "floor_ceiling"
+    res.color_mask.layout.dim[1].size = 2
+    res.color_mask.layout.dim[1].stride = 2*3
+    res.color_mask.layout.dim[2].label = "h,s,v"
     res.color_mask.layout.dim[2].size = 3
     res.color_mask.layout.dim[2].stride = 3
 
     #print ("Color mask sent.")
     
     return res
-
 
 ##############################################################
 # color_mask_server()
